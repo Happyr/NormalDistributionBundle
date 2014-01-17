6b68faf876c9b4a1d084c70194ecdecd35255ac6
Nyholm <tobias.nyholm@gmail.com>
2014-01-17 16:08:04 +0100

    typo

--- Tests/Services/StatisticsServiceTest.php
+++ Tests/Services/StatisticsServiceTest.php
@@ -34,42 +34,42 @@ class StatisticsServiceTest extends \PHPUnit_Framework_TestCase
         }
     }
 
-    public function testGetStanineForProcentile()
+    public function testgetStanineForPercentile()
     {
         $service = new StatisticsService();
 
-        $this->assertEquals(1, $service->getStanineForProcentile(-2));
-        $this->assertEquals(1, $service->getStanineForProcentile(0));
-        $this->assertEquals(2, $service->getStanineForProcentile(4));
-        $this->assertEquals(2, $service->getStanineForProcentile(4.1));
+        $this->assertEquals(1, $service->getStanineForPercentile(-2));
+        $this->assertEquals(1, $service->getStanineForPercentile(0));
+        $this->assertEquals(2, $service->getStanineForPercentile(4));
+        $this->assertEquals(2, $service->getStanineForPercentile(4.1));
 
-        $this->assertEquals(2, $service->getStanineForProcentile(10));
-        $this->assertEquals(3, $service->getStanineForProcentile(11));
-        $this->assertEquals(3, $service->getStanineForProcentile(12));
+        $this->assertEquals(2, $service->getStanineForPercentile(10));
+        $this->assertEquals(3, $service->getStanineForPercentile(11));
+        $this->assertEquals(3, $service->getStanineForPercentile(12));
 
-        $this->assertEquals(3, $service->getStanineForProcentile(22));
-        $this->assertEquals(4, $service->getStanineForProcentile(23));
-        $this->assertEquals(4, $service->getStanineForProcentile(24));
+        $this->assertEquals(3, $service->getStanineForPercentile(22));
+        $this->assertEquals(4, $service->getStanineForPercentile(23));
+        $this->assertEquals(4, $service->getStanineForPercentile(24));
 
-        $this->assertEquals(4, $service->getStanineForProcentile(39));
-        $this->assertEquals(5, $service->getStanineForProcentile(40));
-        $this->assertEquals(5, $service->getStanineForProcentile(41));
+        $this->assertEquals(4, $service->getStanineForPercentile(39));
+        $this->assertEquals(5, $service->getStanineForPercentile(40));
+        $this->assertEquals(5, $service->getStanineForPercentile(41));
 
-        $this->assertEquals(5, $service->getStanineForProcentile(59));
-        $this->assertEquals(6, $service->getStanineForProcentile(60));
-        $this->assertEquals(6, $service->getStanineForProcentile(61));
+        $this->assertEquals(5, $service->getStanineForPercentile(59));
+        $this->assertEquals(6, $service->getStanineForPercentile(60));
+        $this->assertEquals(6, $service->getStanineForPercentile(61));
 
-        $this->assertEquals(6, $service->getStanineForProcentile(76));
-        $this->assertEquals(7, $service->getStanineForProcentile(77));
-        $this->assertEquals(7, $service->getStanineForProcentile(78));
+        $this->assertEquals(6, $service->getStanineForPercentile(76));
+        $this->assertEquals(7, $service->getStanineForPercentile(77));
+        $this->assertEquals(7, $service->getStanineForPercentile(78));
 
-        $this->assertEquals(7, $service->getStanineForProcentile(88));
-        $this->assertEquals(8, $service->getStanineForProcentile(89));
-        $this->assertEquals(8, $service->getStanineForProcentile(90));
+        $this->assertEquals(7, $service->getStanineForPercentile(88));
+        $this->assertEquals(8, $service->getStanineForPercentile(89));
+        $this->assertEquals(8, $service->getStanineForPercentile(90));
 
-        $this->assertEquals(8, $service->getStanineForProcentile(95));
-        $this->assertEquals(9, $service->getStanineForProcentile(96));
-        $this->assertEquals(9, $service->getStanineForProcentile(97));
-        $this->assertEquals(9, $service->getStanineForProcentile(104));
+        $this->assertEquals(8, $service->getStanineForPercentile(95));
+        $this->assertEquals(9, $service->getStanineForPercentile(96));
+        $this->assertEquals(9, $service->getStanineForPercentile(97));
+        $this->assertEquals(9, $service->getStanineForPercentile(104));
     }
 }
